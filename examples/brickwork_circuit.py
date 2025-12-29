909894727a82a71d17bbb33a8e98af5e432c544c
hugo <loio.2601@gmail.com>
2025-12-29 23:27:40 +0000

    Final brickwork circuit

--- examples/brickwork_circuit.py
+++ examples/brickwork_circuit.py
@@ -8,13 +8,16 @@ D = 3 # Number of layers (even and odd)
 
 hs = 1.3 # Horizontal spacing between qubits
 vs = 1.7 # Vertical spacing between layers
-vs0 = 1.7 # Vertical spacing between the qubits and the first layer
 
 strokewidth = 0.15
 
-gate = td.Rectangle(2, 1.3, fc = 'blue', sw = strokewidth)
+height = 1.3 # Gate width
+width  = 2  # Gate height
+gate = td.Rectangle(width, height, fc = 'blue', sw = strokewidth)
+
 qubit = td.Circle(0.5, fc = (0.5,0.5,0.5), sw = strokewidth)
-qubit.add_leg(np.pi/2)
+# The long legs of the qubit tensors act as the physical dofs
+qubit.add_leg(np.pi/2, length = (2*D+0.4)*vs)
 
 fig = td.Figure()
 
@@ -22,12 +25,11 @@ fig = td.Figure()
 for i in range(N):
     fig.place(qubit, hs*i, 0)
 
+# Place gates
 for l in range(D):
     for i in range(0, N-1, 2):
-        fig.place(gate, hs*i + hs/2, vs0 + 2*l*vs)
+        fig.place(gate, hs*i + hs/2, (2*l+1)*vs)
     for i in range(0, N-2, 2):
-        fig.place(gate, hs*(i+1) + hs/2, vs0 + 2*l*vs + vs)
-
-
+        fig.place(gate, hs*(i+1) + hs/2, (2*l+2)*vs)
 
 fig.export("brickwork_circuit.pdf")
